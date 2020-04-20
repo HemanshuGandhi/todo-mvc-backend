9c0c2e99b78ea486299dab9e662b0d831072f2e5
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-04-20 23:55:09 +0800

    Pass spec for delete requests

--- index.js
+++ index.js
@@ -17,4 +17,24 @@ app.get('/api/todos', (req, res) => {
   res.send(todoArray);
 })
 
+app.delete('/api/todos', (req, res) => {
+  todos.splice(0, todos.length);
+  // Respond with empty array
+  res.send(todos);
+})
+
+app.delete('/api/todos:id', (req, res) => {
+  // Look up the todo
+  const todo = todos.find(todo => todo.ID === parseInt(req.params.id));
+  // Not existing, return 404
+  if (!todo) {
+    res.status(404).send('The todo with given ID was not found.')
+  }
+  // Delete the todo
+  const index = todos.indexOf(todo);
+  todos.splice(index, 1);
+  // Respond with deleted todo, by convention.
+  res.send(todo);
+})
+
 app.listen(3000, () => console.log('listening on port 3000...'));
